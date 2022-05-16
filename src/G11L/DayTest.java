ec32e416f586bd6da4a5c9249d4dc8e1879cc424
monya-9 <lee99714@naver.com>
2022-05-16 11:42:03 +0900

    인터페이스

---
+++ src/G11L/DayTest.java
@@ -0,0 +1,11 @@
+package G11L;
+
+interface Days {
+	public static final int SUNDAY = 1, MONDAY = 2, TUESDAY = 3, WEDNESDAY = 4, THURSDAY = 5, FRIDAY = 6, SATURDAY = 7;
+}
+
+public class DayTest implements Days{
+	public static void main(String[] args) {
+		System.out.println("¿œø‰¿œ: " + SUNDAY);
+	}
+}
