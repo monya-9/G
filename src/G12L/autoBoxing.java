a9399a9bb0b43dbac42c70af6fb0c04db99e3109
monya-9 <lee99714@naver.com>
2022-05-24 10:24:33 +0900

    오토박싱

--- src/G12L/autoBoxing.java
+++ src/G12L/autoBoxing.java
@@ -5,5 +5,11 @@ public class autoBoxing {
 		Integer box;
 		box = 10;
 		System.out.println(box + 1);
+		//밑에와 같은 값이 나옴
+		//오토박싱으로 위 처럼 써도 무방하다는것
+		
+//		Integer box;
+//		box = new Integer(10);
+//		System.out.println(box.intValue() + 1);
 	}
 }
