398b75270251013290e7f5ab7a0e818cca1886fd
monya-9 <lee99714@naver.com>
2022-05-24 10:24:44 +0900

    StringBuffer

---
+++ src/G12L/stringBuffer.java
@@ -0,0 +1,12 @@
+package G12L;
+
+public class stringBuffer {
+	public static void main(String[] args) {
+		StringBuffer s = new StringBuffer("Happiness depends upon ourselves");
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("Hello");
+		
+		System.out.println(s + ", " + sb);
+	}
+}
